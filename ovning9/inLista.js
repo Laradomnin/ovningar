e5cf49a13db3720272405d747753eccfd9674941
Laradomnin <lara.domnin@hotmail.com>
2022-06-26 12:12:15 +0200

    .....

---
+++ ovning9/inLista.js
@@ -0,0 +1,8 @@
+//document.querySelector('button').onclick = myClick;
+document.querySelector('button').addEventListener('click',myClick);
+function myClick(){
+    console.log('work');
+    let a = document.querySelector('.i1').value;
+    console.log (a);
+    document.querySelector('.out').innerHTML+=a;
+}
---
+++ ovning9/lista2.js
@@ -0,0 +1,25 @@
+//document.querySelector('button').onclick = addLi();
+
+/*function addLi(){
+    var textVal= document.getElementsById('textVal').value, 
+    listNode = document.getElementById('list'),
+                    liNode = document.createElement('LI'),
+                    txtNode = document.createTextNode(textVal);
+            
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+            
+}*/
+
+function addLi()
+            {
+
+                var txtVal = document.getElementById('txtVal').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+            
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+            
+            }
